60dad7206f62b02f06ae839e6ff2ab2a40f0ea41
sj1338 <id177@naver.com>
2021-02-16 13:09:25 +0900

    계속 기본 세팅중

--- src/main/java/com/ssm/app/controller/HomeController.java
+++ src/main/java/com/ssm/app/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.ssm.app;
+package com.ssm.app.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -19,21 +19,11 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String event(Locale locale, Model model) {
 		
 		return "home";
 	}
+	
 	
 }
